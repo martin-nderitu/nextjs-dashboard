5d00e8a5d43dac2201cac46a372554ce4c46b4e5
Martin Nderitu <45202018+martin-nderitu@users.noreply.github.com>
2024-11-11 16:52:44 +0300

    Chapter 17

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -2,11 +2,14 @@ import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
 import Table from "@/app/ui/invoices/table";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
-import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
-import {SearchParams} from "next/dist/server/request/search-params";
 import { fetchInvoicesPages } from "@/app/lib/data";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Invoices",
+};
 
 export default async function Page(props: {
     searchParams?: Promise<{
